cce563a98293b7082ec46fea248b59f875ee6605
DaweiShen1 <65289649+DaweiShen1@users.noreply.github.com>
2020-06-03 16:56:14 +0900

    Update gemm.cu

--- 07_cache_gpu/cutlass/gemm.cu
+++ 07_cache_gpu/cutlass/gemm.cu
@@ -13,12 +13,9 @@
 using namespace cutlass;
 
 int main(int argc, const char **argv) {
-  int m = 10240;
-  int k = 4096;
-  int n = 4096;
-  float alpha = 1.0;
-  float beta = 0.0;
-  static const matrix_transform_t::kind_t TransformA = matrix_transform_t::NonTranspose;
+  int m = 10240, k = 4096, n = 4096;
+  float alpha = 1.0, beta = 0.0;
+    static const matrix_transform_t::kind_t TransformA = matrix_transform_t::NonTranspose;
   static const matrix_transform_t::kind_t TransformB = matrix_transform_t::NonTranspose;
   typedef float value_t;
   typedef float accum_t;
@@ -32,10 +29,6 @@ int main(int argc, const char **argv) {
   B.random();
   C.fill_ramp(0,0);
   C2.fill_ramp(0,0);
-  A.sync_device();
-  B.sync_device();
-  C.sync_device();
-  C2.sync_device();
   cublasHandle_t g_cublas_handle;
   cublasCreate(&g_cublas_handle);
   gpu_timer timer;
@@ -62,7 +55,6 @@ int main(int argc, const char **argv) {
   double tcublas = timer.elapsed_millis() / g_timing_iterations;
   double cublas_flops = double(num_flops) / tcublas / 1.0e6;
   typedef gemm::blas_scaled_epilogue<float, float, float> epilogue_op_t;
-  epilogue_op_t epilogue(alpha, beta);
   for (int i = 0; i < g_timing_iterations+2; i++) {
     if (i == 2) timer.start();
     gemm::dispatch<epilogue_op_t>(
@@ -81,8 +73,6 @@ int main(int argc, const char **argv) {
   double tcutlass = timer.elapsed_millis() / g_timing_iterations;
   double cutlass_flops = double(num_flops) / tcutlass / 1.0e6;
   printf("CUBLAS: %.2f Gflops, CUTLASS: %.2f Gflops\n", cublas_flops, cutlass_flops);
-  C.sync_host();
-  C2.sync_host();
   double err = 0;
   for (int i=0; i<n; i++) {
     for (int j=0; j<m; j++) {
@@ -92,3 +82,4 @@ int main(int argc, const char **argv) {
   printf("error: %lf\n", err/n/m);
   cublasDestroy(g_cublas_handle);
 }
+
